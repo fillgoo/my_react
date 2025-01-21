1afbbafd0481c14e2a1412f3c6e92a0ebe118cf0
fillgoo <fillbox@naver.com>
2025-01-21 12:48:16 +0900

    1_2 components 추가

--- src/App.js
+++ src/App.js
@@ -26,9 +26,22 @@ import React from 'react';
 // }
 
 // export default App;
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import HomeView from './Views/HomeView';
+import AboutView from './Views/AboutView';
 
 const App = ()=>{
-  return <div>App</div>
+  // return <div>App</div> -- 한줄일 때 
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomeView />} />
+      <Route path='/about' element={<AboutView />} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
+// BrowserRouter : 라우팅을 적용하기 위한 컴포넌트입니다. App 컴포넌트를 감싸서 라우팅 설정을 적용할 수 있는 명령어
+//Routes : 라우터들을 정의하는 컨테이너입니다. 명령어를 가지고 있습니다. 
+// /* <Route path='/' element={<HomeView />} /> */ : '경로에 해당하는 <HomeView />' 컴포넌트를 랜더링하도록 설정
 export default App;
